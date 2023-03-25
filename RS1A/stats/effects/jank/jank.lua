f4ce82c09c5f062441b42bab857507a2d133f9b8
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2023-03-25 17:47:37 -0500

    Update jank.lua

--- RS1A/stats/effects/jank/jank.lua
+++ RS1A/stats/effects/jank/jank.lua
@@ -1,17 +1,17 @@
-require "/scripts/util.lua" --i suck at doing starb
+require "/scripts/util.lua" -- i suck at doing starb
 require "/scripts/status.lua"
 
 function init()
-    status.addPersistentEffects("maso", {"masochism"}) --yeahh
+    status.addPersistentEffects("maso", {"masochism"}) -- yeahh
     world.sendEntityMessage(entity.id(), "queueRadioMessage", "masochismPrepareThyself")
     player.giveBlueprint("parryshield")
 
 end
 
 function update(dt)
-    --nothing really happens its jsut to get the effect up
+    -- nothing really happens its jsut to get the effect up
 end
 
 function uninit()
-    --also nothing
+    -- also nothing
 end
