0ace11881a448e98658f192a1e24eb8132335f25
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2023-03-25 23:32:06 -0500

    Update jank.lua

--- RS1A/stats/effects/jank/jank.lua
+++ RS1A/stats/effects/jank/jank.lua
@@ -3,7 +3,13 @@ require "/scripts/status.lua"
 
 function init()
     -- nothing
+    local pain = {
+        name = "parryshield"
+    }
 
+    status.addPersistentEffects("maso", {"masochism"}) -- yeahh
+    world.sendEntityMessage(entity.id(), "queueRadioMessage", "masochismPrepareThyself")
+    player.giveBlueprint(pain)
 end
 
 function update(dt)
