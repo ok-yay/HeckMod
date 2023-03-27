09aaa98fb6620d1d9f0ef9b4a824eaacf3bdf895
kakaeater33 <77019279+kakaeater33@users.noreply.github.com>
2023-03-26 19:45:55 -0500

    Update jank.lua

--- RS1A/stats/effects/jank/jank.lua
+++ RS1A/stats/effects/jank/jank.lua
@@ -2,12 +2,11 @@ require "/scripts/util.lua" -- i suck at doing starb
 require "/scripts/status.lua"
 
 function init()
-    -- testing if update() does anything different
 
     status.addPersistentEffects("maso", {"masochism"}) -- yeahh
     world.sendEntityMessage(entity.id(), "queueRadioMessage", "masochismPrepareThyself")
     world.spawnItem("parryshield-recipe", entity.position(), 1)
-    status.setStatusProperty("hell",1) --id use player.getStat but player is so FUCKING annoying to get working due to its stupidly limited access
+    status.setStatusProperty("hellActive",1) --id use player.getStat but player is so FUCKING annoying to get working due to its stupidly limited access
 
 end
 
