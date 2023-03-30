e8cfdb1121f726f28e3b810d237fdd95720bbc05
kakaeater33 <77019279+kakaeater33@users.noreply.github.com>
2023-03-29 23:46:16 -0500

    Update harddmg.lua

--- RS1A/interface/harddmg/harddmg.lua
+++ RS1A/interface/harddmg/harddmg.lua
@@ -2,6 +2,8 @@ require "/scripts/util.lua"
 
 function init()
   player.giveBlueprint("parryshield")
+  widget.setProgress("prgCurrentProgress", 0.5)
+  widget.blur("background")
 end
 
 function update(dt)
