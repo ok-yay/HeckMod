d4978553a292c55a79d4faeb1ecca8d91cdca2f0
kakaeater33 <77019279+kakaeater33@users.noreply.github.com>
2023-03-30 17:33:30 -0500

    Update jank.lua

--- RS1A/stats/effects/jank/jank.lua
+++ RS1A/stats/effects/jank/jank.lua
@@ -4,7 +4,7 @@ require "/scripts/status.lua"
 function init()
     world.sendEntityMessage(entity.id(), "interact", "ScriptPane", "/interface/workaround/blueprint.config") -- les goooooo!!!
 
-    status.addPersistentEffects("maso", {"masochism"}) -- yeahh
+    --status.addPersistentEffects("maso", {"masochism"}) -- no longer necessary POG
     world.sendEntityMessage(entity.id(), "queueRadioMessage", "masochismPrepareThyself")
     world.spawnItem("parryshield-recipe", entity.position(), 1)
     status.setStatusProperty("hellActive", 1) -- player is so FUCKING annoying to get working due to its stupidly limited access
