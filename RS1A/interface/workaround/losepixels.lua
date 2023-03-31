6e9053554d218f346150d1b1d907aafbf6804d16
kakaeater33 <77019279+kakaeater33@users.noreply.github.com>
2023-03-30 23:10:49 -0500

    Update losepixels.lua

--- RS1A/interface/workaround/losepixels.lua
+++ RS1A/interface/workaround/losepixels.lua
@@ -2,6 +2,8 @@ require "/scripts/util.lua"
 
 function init()
     player.consumeCurrency("money", math.floor((player.currency("money")*2)/7))
+    world.sendEntityMessage(entity.id(), "queueRadioMessage", "exploreclueplanet")
+
     sb.logInfo(player.id() + " the " + player.species() + " is dumb as shit.")
     pane.dismiss()
 end
