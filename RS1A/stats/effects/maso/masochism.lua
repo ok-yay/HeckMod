dc2bd23ee20f79cc7487a78f8acf083354c3a515
ok yay <72277679+ok-yay@users.noreply.github.com>
2023-04-09 23:08:25 -0500

    Update masochism.lua

--- RS1A/stats/effects/maso/masochism.lua
+++ RS1A/stats/effects/maso/masochism.lua
@@ -10,7 +10,7 @@ function init()
             if notification.hitType == "Hit" then
 
 
-                status.setResource("harddmg", status.resource("harddmg") + notification.damageDealt / 4.5)
+                status.setResource("harddmg", status.resource("harddmg") + notification.damageDealt / 2)
                 self.timer = 200
 
                 if (status.resource("harddmg") > status.resourceMax("health") - 10) then
