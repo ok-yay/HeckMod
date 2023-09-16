a19cd6b08a634c53f07c085604ac40760753055e
ok yay <72277679+ok-yay@users.noreply.github.com>
2023-09-15 23:53:24 -0500

    Update doubledown.lua

--- RS1A/stats/effects/doubleddown/doubledown.lua
+++ RS1A/stats/effects/doubleddown/doubledown.lua
@@ -5,7 +5,17 @@ function init() --hard dmg doesnt go away nearly as easily this time buckaroo!
 end
 
 function update(dt)
-    local math = status.resourceMax("health") - status.resource("harddmg")
+    if (tostring(world.type()) == "unknown") then
+        if status.resource("harddmg") > 0 then
+            status.setResource("harddmg", status.resource("harddmg") - 0.9)
+            status.setResource("health", status.resourceMax("health")-status.resource("harddmg")) --its far clearer whats happenening, since no bar this time around.
+
+        else
+            status.setResource("harddmg", 0)
+            world.sendEntityMessage(entity.id(), "removeBar", "harddamage")
+        end
+    else
+        local math = status.resourceMax("health") - status.resource("harddmg")
     if (status.resource("health") < math) then
         if (math > 10) then
             status.setResource("harddmg", status.resourceMax("health")-status.resource("health"))
@@ -16,15 +26,9 @@ function update(dt)
     if (status.resource("health") > status.resourceMax("health") - status.resource("harddmg")) then
         status.setResource("health", status.resourceMax("health") - status.resource("harddmg"))
     end
-    sb.logInfo(world.type)
-    if (world.type == "unknown") then
-        if status.resource("harddmg") > 0 then
-            status.setResource("harddmg", status.resource("harddmg") - 0.0)
-
-        else
-            status.setResource("harddmg", 0)
-            world.sendEntityMessage(entity.id(), "removeBar", "harddamage")
-        end
+    
+    
+    
     end
 end
 
