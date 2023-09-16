3679ff1d24af4308a91376f74e357fc1b9d20b7c
ok yay <72277679+ok-yay@users.noreply.github.com>
2023-09-15 23:20:12 -0500

    Update doubledown.lua

--- RS1A/stats/effects/doubleddown/doubledown.lua
+++ RS1A/stats/effects/doubleddown/doubledown.lua
@@ -5,15 +5,20 @@ function init() --hard dmg doesnt go away nearly as easily this time buckaroo!
 end
 
 function update(dt)
-    if (status.resource("health") < status.resourceMax("health") - status.resource("harddmg") - 2) then
-        status.setResource("harddmg", status.resourceMax("health")-status.resource("health") - 2)
+    local math = status.resourceMax("health") - status.resource("harddmg")
+    if (status.resource("health") < math) then
+        if (math > 10) then
+            status.setResource("harddmg", status.resourceMax("health")-status.resource("health"))
+        else
+            status.setResource("harddmg", status.resourceMax("health")-10)
+        end
     end
     if (status.resource("health") > status.resourceMax("health") - status.resource("harddmg")) then
         status.setResource("health", status.resourceMax("health") - status.resource("harddmg"))
     end
     if (world.type == "unknown") then
         if status.resource("harddmg") > 0 then
-            status.setResource("harddmg", status.resource("harddmg") - 0.7)
+            status.setResource("harddmg", status.resource("harddmg") - 0.0)
 
         else
             status.setResource("harddmg", 0)
@@ -21,8 +26,6 @@ function update(dt)
 
         end
     end
-
-    self.damageListener:update()
 end
 
 function uninit()
