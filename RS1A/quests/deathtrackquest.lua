b2c66d65445cf992e981d9211cc2fd8ad2684928
ok yay <72277679+ok-yay@users.noreply.github.com>
2025-06-27 14:55:15 -0500

    the UNIQUIE decision to have mechs remove effects is certainly a choice that was made >:(

--- RS1A/quests/deathtrackquest.lua
+++ RS1A/quests/deathtrackquest.lua
@@ -1,7 +1,15 @@
 -- this... is better than opening a fucking gui and then closing it immediately after but i dont like having an always running quest... it feels icky. oh well, shouldnt impact performance since it cannot be completed
 require "/scripts/messageutil.lua"
 
+function isInMech()
+    if (player.loungingIn() ~= nil) then
+        return world.entityName(player.loungingIn()) == "modularmech"
+    end
+    return false
+end
+
 function init()
+    self.mechTime = isInMech()
     self.broYouDied = false
     self.timeTilFail = 0
     self.timeSinceInit = 0 -- incase the effect goes away somehow after init
@@ -42,20 +50,21 @@ function resetCount()
 end
 
 function potentialDeath()
-    sb.logInfo(tostring(self.timeSinceInit<3 and player.isDeployed() == false and storage.mechStuff == false))
-     if ((status.stat("hellActive") > 0.0 or status.stat("flourEater") > 0.0) and self.timeSinceInit<3 and player.isDeployed() == false and storage.mechStuff == false) then
+    --sb.logInfo(tostring(isInMech()) .. tostring(storage.mechStuff))
+    if ((status.stat("hellActive") > 0.0 or status.stat("flourEater") > 0.0) and self.timeSinceInit<3 and isInMech() == false and storage.mechStuff == false) then
         sb.logInfo("Server should soon say that you died")
         self.broYouDied = true
     end
 end
 
 function update()
-    storage.mechStuff = (storage.mechStuff or player.isDeployed())  -- if mechStuff isn't being stored/is nill
+    if (storage.mechStuff == nil) then --idk why the fuck its getting freaky sauce
+        storage.mechStuff = true
+    end
     if (status.stat("notDead") == 0.0) then -- readds the effect
         potentialDeath()
         status.addEphemeralEffect("deathTracker",10)
     end
-
     if (self.broYouDied == true) then
         sb.logInfo("...any second now it should broadcast it...")
         storage.amountYouHaveDied = (storage.amountYouHaveDied or 0)+1 -- in theory at least
@@ -65,7 +74,6 @@ function update()
         })
         self.broYouDied = false
     end
-
     if (status.resource("hellActive") == 0.0 and status.resource("flourEater") == 0.0) then
         self.timeTilFail = self.timeTilFail + 1
     end
@@ -77,7 +85,7 @@ function update()
     if (self.timeSinceInit<10) then -- since delta 60
         self.timeSinceInit = self.timeSinceInit + 1 -- i assume += exists but im too lazy to actually figure that out
     end
-    storage.mechStuff = player.isDeployed()
+    storage.mechStuff = isInMech()
 end
 
 function blowUp() -- hopefully shouldn't need to check in update but i may have to depending on circumstances
@@ -94,12 +102,9 @@ end
 
 --[[
 
-ok so here's as good of an explanation i can give for how this hunk of shit works:
+THE COMPLAINING CORNER
 
-basically ephemeral effects go away on death and you can cause an effect to constantly increase its own duration 
-so that it never go away on its own. stat modifiers are silly and seem to be a million times more consistent than
-resource (TODO: make it so that flourEater and hellActive are tracked via stat modifiers) so that's pretty neat.
-the reason this is all in a quest is to deal with the inefficiency (and jank) of constantly making scriptpanes to 
-do really anything that required the player table. that, and i also wanted access to the storage table as well.
+player.isDeployed() is misleading as fuck dude
+why the shit are mechs the most fucked things in the world to deal with????
 
 ]]
